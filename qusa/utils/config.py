b6e2c1dfcc9296f4870ae18e32486c827d59945b
ranasid17 <39922327+ranasid17@users.noreply.github.com>
2025-12-25 22:17:34 -0600

    move file

---
+++ qusa/utils/config.py
@@ -0,0 +1,32 @@
+# qusa/utils/config.py 
+
+import os 
+import yaml
+
+from pathlib import Path
+
+
+def load_config(config_path='config.yaml'):
+    """
+    Load configuration from a YAML file.
+
+    Parameters: 
+        1) config_path (str): Path to the YAML configuration file.
+    """
+
+    config_path = Path(config_path).expanduser()
+
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # expand user paths
+    for section in config.values(): 
+        if isinstance(section, dict): 
+            for key, value in section.items(): 
+                if isinstance(value, str) and value.startswith('~'):
+                    section[key] = os.path.expanduser(value)
+                if isinstance(value, dict): 
+                    for subkey, subvalue in value.items(): 
+                        if isinstance(subvalue, str) and subvalue.startswith('~'):
+                            section[key][subkey] = os.path.expanduser(subvalue)
+    return config
