de4cf9bb72b844e1833f88c17148d7a190ddeadc
ranasid17 <39922327+ranasid17@users.noreply.github.com>
2026-01-02 13:05:34 -0500

    add backtest/eval/train LLM helper methods to public scope

--- qusa/model/__init__.py
+++ qusa/model/__init__.py
@@ -6,14 +6,20 @@ Machine learning model for overnight price prediction
 
 from .backtest import ModelBacktester
 from .evaluate import evaluate_model
-from .reporter import generate_evaluation_report
+from .reporter import (
+    generate_backtest_report,
+    generate_evaluation_report,
+    generate_training_report,
+)
 from .predict import make_prediction, LivePredictor
 from .train import train_model, OvernightDirectionModel
 
 __all__ = [
     "ModelBacktester",
     "evaluate_model",
+    "generate_backtest_report",
     "generate_evaluation_report",
+    "generate_training_report",
     "LivePredictor",
     "train_model",
     "make_prediction",
