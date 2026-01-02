ffad9439f1e5ac5806bd90e02ea7cea51992a722
ranasid17 <39922327+ranasid17@users.noreply.github.com>
2026-01-02 12:55:38 -0500

    exposed LLM module for import

--- qusa/model/__init__.py
+++ qusa/model/__init__.py
@@ -6,12 +6,14 @@ Machine learning model for overnight price prediction
 
 from .backtest import ModelBacktester
 from .evaluate import evaluate_model
+from .reporter import generate_evaluation_report
 from .predict import make_prediction, LivePredictor
 from .train import train_model, OvernightDirectionModel
 
 __all__ = [
     "ModelBacktester",
     "evaluate_model",
+    "generate_evaluation_report",
     "LivePredictor",
     "train_model",
     "make_prediction",
